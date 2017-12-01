566953662d632ae178db277e599579fa28a9aaf1
SYDNEY\stuart <strt.mcroberts@gmail.com>
2017-12-01 13:03:46 +1100

    level generator almost done

--- CardGameStrategy/Assets/Scripts/Tools/MapCreator/MapButtons.cs
+++ CardGameStrategy/Assets/Scripts/Tools/MapCreator/MapButtons.cs
@@ -6,38 +6,55 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 namespace Assets.Scripts.Tools.MapCreator
 {
     class MapButtons:MonoBehaviour
     {
         public Button button;
 
+        private void Awake()
+        {
+            button = this.GetComponent<Button>();
+
+            button.onClick.AddListener(OnClick);
+            button.image.color = Color.gray;
+        }
+
         public BlockType type;
+
+        public void setColor()
+        {
+            switch (type)
+            {
+                case BlockType.Normal:
+                    button.image.color = Color.gray;
+                    break;
+                case BlockType.Fire:
+                    button.image.color = Color.red;
+                    break;
+                case BlockType.Water:
+                    button.image.color = Color.blue;
+                    break;
+                case BlockType.Grass:
+                    button.image.color = Color.green;
+                    break;
+            }
+        }
+
         public void OnClick()
         {
             int x = (int)type;
             x++;
-            try
-            {
-                type = (BlockType)x;
-
-            }
-            catch
+            BlockType lastBlock = Enum.GetValues(typeof(BlockType)).Cast<BlockType>().Last();
+            if (x > (int)lastBlock)
             {
                 x = 0;
-                type = (BlockType)x;
             }
-            switch (type)
-            {
-                case BlockType.Normal:
-                    break;
-                case BlockType.Fire:
-                    break;
-                case BlockType.Water:
-                    break;
-                case BlockType.Grass:
-                    break;
-            }
+             type = (BlockType)x;
+
+
+            setColor();
         }
 
     }
--- CardGameStrategy/Assets/Scripts/Tools/MapCreator/MapJsonGenerator.cs
+++ CardGameStrategy/Assets/Scripts/Tools/MapCreator/MapJsonGenerator.cs
@@ -1,15 +1,67 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Tools.MapCreator
 {
     class MapJsonGenerator:MonoBehaviour
     {
+        public InputField HeightInput;
+        public int gridHeight
+        {
+            get
+            {
+                int x = 0;
+                int.TryParse(HeightInput.text,out x);
+                return x;
+            }
+        }
+        public InputField WidthInput;
+        public int gridwidth
+        {
+            get
+            {
+                int x = 0;
+                int.TryParse(WidthInput.text, out x);
+                return x;
+            }
+        }
+
+
+
         public MapButtons[] buttonArray;
         public List<List<MapButtons>> buttons = new List<List<MapButtons>>();
+
+        IEnumerator colorCo()
+        {
+            for (int i = 0; i < buttons.Count && i < gridHeight; i++)
+            {
+                for (int f = 0; f < buttons[0].Count && f < gridwidth; f++)
+                {
+                    buttons[i][f].button.image.color = Color.cyan;
+                }
+            }
+            yield return new WaitForSeconds(2f);
+
+            for (int i = 0; i < buttons.Count && i < gridHeight; i++)
+            {
+                for (int f = 0; f < buttons[0].Count && f < gridwidth; f++)
+                {
+                    buttons[i][f].setColor();
+                }
+            }
+        }
+
+        public void showGridClick()
+        {
+            StartCoroutine(colorCo());
+        }
+
+
 
         void Awake()
         {
